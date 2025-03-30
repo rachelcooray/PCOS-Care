2bbacadcd3144d848a27d440dc5c82bbc6c98820
rachelcooray <rachelcooray@gmail.com>
2025-03-30 00:27:36 +0000

    ui fix: dashboards

--- frontend/.ipynb_checkpoints/pcos_dashboard-checkpoint.py
+++ frontend/.ipynb_checkpoints/pcos_dashboard-checkpoint.py
@@ -50,7 +50,7 @@ def display_image_and_explanation(image_filename, caption, explanation):
         # Explanation within an expander
         with st.expander(f"Learn more about {caption}"):
             st.markdown(f"""
-                <div style='background-color: #f0f8ff; padding: 15px; border-radius: 10px;'>
+                <div style='background-color: #D6C2EA; padding: 15px; border-radius: 10px; margin-bottom: 15px;'>
                     <p style='color: #333; font-size: 14px; line-height: 1.6;'>{explanation}</p>
                 </div>
             """, unsafe_allow_html=True)
@@ -70,7 +70,7 @@ def display_image_and_explanation_large(image_filename, caption, explanation):
     
     with st.expander(f"Learn more about {caption}"):
         st.markdown(f"""
-                <div style='background-color: #f0f8ff; padding: 15px; border-radius: 10px;'>
+                <div style='background-color: #D6C2EA; padding: 15px; border-radius: 10px; margin-bottom: 15px;'>
                     <p style='color: #333; font-size: 14px; line-height: 1.6;'>{explanation}</p>
                 </div>
             """, unsafe_allow_html=True)
@@ -89,8 +89,8 @@ def pcos_dashboard_page():
     
     # Introductory Overview of PCOS
     st.markdown("""
-        <div style='background-color: #f0f8ff; padding: 20px; border-radius: 10px; margin-bottom: 20px;'>
-            <h1 style='color: #6a0dad;'>Welcome to Your PCOS Journey</h1>
+        <div style='background-color: #E5D9F2; padding: 20px; border-radius: 10px; margin-bottom: 20px;'>
+            <h1 style='color: #947bd6; text-align: center;'>Welcome to the Insights Dashboard</h1>
             <p>In this section, you will be taken through the impact of Polycystic Ovary Syndrome (PCOS), how it affects millions of people, and the ways you can manage it effectively. You will have a deeper understanding of how PCOS can shape your life, along with practical insights on symptoms and the role of exercise.</p>
         </div>
     """, unsafe_allow_html=True)
@@ -98,7 +98,7 @@ def pcos_dashboard_page():
     # 1: The Growing Challenge - PCOS Prevalence
     st.markdown("""
         <div style='background-color: #f9f9f9; padding: 20px; border-radius: 10px; margin-bottom: 20px;'>
-            <h2 style='color: #6a0dad;'>Insight 1: The Growing Challenge - PCOS Prevalence</h2>
+            <h2 style='color: #9C89FB;'>Insight 1: The Growing Challenge - PCOS Prevalence</h2>
             <p>PCOS affects millions of people worldwide, yet it remains widely underdiagnosed. The first step toward better management of this condition is understanding its prevalence. This shows the alarming number of undiagnosed cases.</p>
         </div>
     """, unsafe_allow_html=True)
@@ -114,7 +114,7 @@ def pcos_dashboard_page():
     # 2: The Faces of PCOS - Symptoms Breakdown
     st.markdown("""
         <div style='background-color: #f9f9f9; padding: 20px; border-radius: 10px; margin-bottom: 20px;'>
-            <h2 style='color: #6a0dad;'>Insight 2: The Faces of PCOS - Common Symptoms</h2>
+            <h2 style='color: #9C89FB;'>Insight 2: The Faces of PCOS - Common Symptoms</h2>
             <p>PCOS affects a significant number of individuals worldwide, but many are still undiagnosed. Despite its prevalence, awareness remains low, making it challenging for people to seek the help they need. This section, shows the widespread nature of PCOS, shedding light on how common it is and the importance of early diagnosis and intervention. Understanding its impact is the first step toward effective management and care.</p>
         </div>
     """, unsafe_allow_html=True)
@@ -131,7 +131,7 @@ def pcos_dashboard_page():
     # 3: The Faces of PCOS - Symptoms Breakdown (contd.)
     st.markdown("""
         <div style='background-color: #f9f9f9; padding: 20px; border-radius: 10px; margin-bottom: 20px;'>
-            <h2 style='color: #6a0dad;'>Insight 3: The Faces of PCOS - Common Symptoms Breakdown</h2>
+            <h2 style='color: #9C89FB;'>Insight 3: The Faces of PCOS - Common Symptoms Breakdown</h2>
             <p>PCOS presents itself through a variety of symptoms that can significantly affect the quality of life. Among the most frequently reported symptoms are pimples, weight gain, skin darkening, hair loss, and excessive hair growth. Understanding these symptoms is crucial for individuals to seek the appropriate treatment. Lifestyle changes, including diet and exercise, can help manage these symptoms and improve overall well-being.</p>
         </div>
     """, unsafe_allow_html=True)
@@ -146,7 +146,7 @@ def pcos_dashboard_page():
     # 4: Managing PCOS - The Role of Exercise
     st.markdown("""
         <div style='background-color: #f9f9f9; padding: 20px; border-radius: 10px; margin-bottom: 20px;'>
-            <h2 style='color: #6a0dad;'>Insight 4: Managing PCOS - The Role of Exercise</h2>
+            <h2 style='color: #9C89FB;'>Insight 4: Managing PCOS - The Role of Exercise</h2>
             <p>Physical activity is one of the most powerful tools in managing PCOS. In this we see how different levels of exercise can positively impact the severity of symptoms.</p>
         </div>
     """, unsafe_allow_html=True)
@@ -161,7 +161,7 @@ def pcos_dashboard_page():
     # Chapter 5: The Impact of Diet - Fast Food and PCOS Symptoms
     st.markdown("""
         <div style='background-color: #f9f9f9; padding: 20px; border-radius: 10px; margin-bottom: 20px;'>
-            <h2 style='color: #6a0dad;'>Insight 5: The Impact of Diet - Fast Food and PCOS Symptoms</h2>
+            <h2 style='color: #9C89FB;'>Insight 5: The Impact of Diet - Fast Food and PCOS Symptoms</h2>
             <p>Diet plays a key role in managing PCOS. In this we can see how a diet rich in fast food may contribute to the worsening of PCOS symptoms.</p>
         </div>
     """, unsafe_allow_html=True)
